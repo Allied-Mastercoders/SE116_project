8f53fa6cf7089930899a120957850b0fe0df1672
F4Y0 <emresenli08@gmail.com>
2026-05-21 20:46:32 +0300

    Add files via upload
    i ve j değiştirildi

--- src/Cell.java
+++ src/Cell.java
@@ -1,18 +1,6 @@
-abstract class Cell {
-    protected int x, y;
-    protected char type;
-
-    public Cell(int x, int y, char type){
-        this.x = x;
-        this.y = y;
-        this.type = type;
-    }
-
-    public char getType() {return type;}
-    public int getX() {return x;}
-    public int getY() {return y;}
-
+public abstract class Cell {
 
 }
-class Road extends Cell { public Road(int x, int y) { super(x, y, 'R'); } }
-class Empty extends Cell { public Empty(int x, int y) { super(x, y, 'E'); } }
+abstract class Zones extends Cell {
+
+}
