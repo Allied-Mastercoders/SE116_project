0664f7b7649abc18dafe3af8097d6d123b2aaf11
BugraKaanBatir <bugrabatir@gmail.com>
2026-06-01 22:28:27 +0300

    Add files via upload

--- src/Commercial.java
+++ src/Commercial.java
@@ -13,7 +13,7 @@ public class Commercial extends Zone{
         } else {
             boolean canBeLevel1 = (populationPool > 0 && goodsPool > 0);
             boolean canBeLevel2 = canBeLevel1 && hasSecurity;
-            boolean canBeLevel3 = canBeLevel2;
+            boolean canBeLevel3 = canBeLevel2 && populationPool>1;
 
             int targetLevel;
 
