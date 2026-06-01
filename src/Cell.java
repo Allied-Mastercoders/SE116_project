0d47c417e6a837bf30edb99f5a052651915ba492
BugraKaanBatir <bugrabatir@gmail.com>
2026-06-01 20:25:44 +0300

    Add files via upload

--- src/Cell.java
+++ src/Cell.java
@@ -1,6 +1,18 @@
-public abstract class Cell {
+abstract class Cell {
+    protected int x, y;
+    protected char type;
+
+    public Cell(int x, int y, char type){
+        this.x = x;
+        this.y = y;
+        this.type = type;
+    }
+
+    public char getType() {return type;}
+    public int getX() {return x;}
+    public int getY() {return y;}
+
 
 }
-abstract class Zones extends Cell {
-
-}
+class Road extends Cell { public Road(int x, int y) { super(x, y, 'R'); } }
+class Empty extends Cell { public Empty(int x, int y) { super(x, y, 'E'); } }
