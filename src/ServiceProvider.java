f83f599333f378c9806fc26d6ce882223e48384a
BugraKaanBatir <bugrabatir@gmail.com>
2026-06-01 20:50:29 +0300

    Add files via upload

--- src/ServiceProvider.java
+++ src/ServiceProvider.java
@@ -1,9 +1,11 @@
 abstract class ServiceProvider extends Cell{
-    protected int radius = 5;
+    protected int radius;
     public ServiceProvider(int x, int y, char type) { super(x, y, type); }
 
     public int getRadius() {return radius;}
 }
-class PoliceStation extends ServiceProvider { public PoliceStation(int x, int y) { super(x, y, 'F'); } }
-class Hospital extends ServiceProvider { public Hospital(int x, int y) { super(x, y, 'D'); } }
-class School extends ServiceProvider { public School(int x, int y) { super(x, y, 'S'); } }
+class PoliceStation extends ServiceProvider { public PoliceStation(int x, int y) { super(x, y, 'F');this.radius=5;}}
+
+class Hospital extends ServiceProvider { public Hospital(int x, int y) { super(x, y, 'D');this.radius=3; } }
+
+class School extends ServiceProvider { public School(int x, int y) { super(x, y, 'S');this.radius=4; } }
